7f73d1d96d68ef251205e587da48b5e193154ab5
Aryaman Swami <swamiaryaman21.as@gmail.com>
2022-01-19 16:26:09 +0530

    sections

---
+++ assets/js/about.js
@@ -0,0 +1,94 @@
+var aarr = window.location.href.split("=");
+//get last value
+var book_id = aarr[aarr.length - 1];
+const bookdetailid = "https://www.googleapis.com/books/v1/volumes";
+const API_KEY = "AIzaSyABCMotJsaZVpPDKaB03v72YRoWAkmiuvo";
+
+// fetching book details
+fetch(`${bookdetailid}/${book_id}`)
+    .then((res) => res.json())
+    .then((data) => {
+        setupbookInfo(data);
+    });
+
+const setupbookInfo = (data) => {
+    const bookName = document.querySelector("#book-name");
+    const authors = document.querySelector("#authors");
+    const genres = document.querySelector(".genres");
+    const desc = document.querySelector(".desc");
+    const title = document.querySelector("title");
+    const subtitle = document.querySelector(".subtitle");
+    const img = document.querySelector(".image");
+    const lang = document.querySelector(".lang");
+    const publisher = document.querySelector(".publisher");
+    const pdate = document.querySelector(".pdate");
+    const rating = document.querySelector(".rating");
+    const mature = document.querySelector(".mature");
+
+    rating.innerHTML = data.volumeInfo.averageRating
+        ? data.volumeInfo.averageRating + "/5"
+        : "Not Rated";
+
+    title.innerHTML = bookName.innerHTML = data.volumeInfo.title;
+    genres.innerHTML = `${data.volumeInfo.categories.map((item) => {
+        return item + ",";
+    })} `;
+    lang.innerHTML = data.volumeInfo.language;
+    publisher.innerHTML = data.volumeInfo.publisher;
+    pdate.innerHTML = data.volumeInfo.publishedDate;
+    mature.innerHTML = data.volumeInfo.maturityRating;
+
+    img.src = data.volumeInfo.imageLinks
+        ? data.volumeInfo.imageLinks.thumbnail
+        : "assets/img/cover.jpg";
+
+    desc.innerHTML = data.volumeInfo.description;
+
+    subtitle.innerHTML = data.volumeInfo.subtitle
+        ? data.volumeInfo.subtitle
+        : "";
+    authors.innerHTML = `${data.volumeInfo.authors.map((item) => {
+        return item;
+    })} `;
+
+    console.log(data.volumeInfo.categories[0]);
+    fetch(`${bookdetailid}?q=${data.volumeInfo.categories[0]}&maxResults=3&key=${API_KEY}`)
+        .then((res) => res.json())
+        .then((data) => {
+            recommendbooks(data);
+        });
+
+    function recommendbooks(data) {
+        const recbooks = document.querySelector("#recbooks");
+        recbooks.innerHTML = "";
+       console.log(data)
+        data.items.forEach((books) => {
+            console.log(books);
+            const { volumeInfo, imageLinks } = books;
+
+            const recimage = document.createElement("div");
+            recimage.classList.add("rec_image");
+            recimage.innerHTML = `
+          <img id="left-image" src=${volumeInfo.imageLinks.thumbnail
+                    ? volumeInfo.imageLinks.thumbnail
+                    : "assets/img/cover.jpg"
+                }>
+            <br>
+            <span class="bookname">${volumeInfo.title.slice(0, 30)} </span>
+            <br>
+            <span class="authorname">${volumeInfo.averageRating
+                    ? volumeInfo.averageRating + "/5"
+                    : "Not Rated"
+                }</span>
+       
+      `
+
+            recbooks.appendChild(recimage);
+        });
+    }
+
+   
+};
+
+
+
---
+++ assets/js/fiction.js
@@ -0,0 +1,166 @@
+const API_KEY = "AIzaSyABCMotJsaZVpPDKaB03v72YRoWAkmiuvo";
+const BASE_URL = `https://www.googleapis.com/books/v1/volumes`;
+const searchURL = `https://www.googleapis.com/books/v1/volumes?q=`;
+// const API_URL = BASE_URL + "?q=fiction&key=" + API_KEY;
+const form = document.getElementById("form");
+const API_URL = `https://www.googleapis.com/books/v1/volumes?&maxResults=15&key=${API_KEY}&startIndex=0`
+const fiction_URL = `https://www.googleapis.com/books/v1/volumes?q=fiction&maxResults=15&key=${API_KEY}&startIndex=0`
+const books = document.getElementById("books");
+const search = document.getElementById("search");
+
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const current = document.getElementById("current");
+// https://www.googleapis.com/books/v1/volumes?q=fiction&key=${API_KEY}
+
+var startIndex;
+var maxResults = 15 ;
+var currentPage =1;
+var totalPages ;
+var searchTerm;
+// getFiction(fiction_URL);
+ loadPage();
+function loadPage() {
+  const oldurl = localStorage.getItem('url');
+  if(oldurl){
+    getFiction(oldurl);
+  }
+ else{
+    getFiction(fiction_URL);
+  }
+}
+// localStorage.getItem("url")?getFiction(lastUrl):getFiction(fiction_URL);
+// getFiction(localStorage.getItem(lastUrl)? lastUrl : fiction_URL)
+function getFiction(url) {
+    lastUrl = url;
+    localStorage.setItem("url",lastUrl);
+    fetch(url).then(res => res.json()).then(data => {
+
+
+        totalPages=data.totalItems;
+        console.log(data);
+        if (data.items.length !== 0) {
+            showBooks(data.items);
+
+                if (currentPage <= 1) {
+                    prev.classList.add('disabled');
+                    next.classList.remove('disabled')
+                }
+                else if (currentPage >= 1 && currentPage <= totalPages) {
+                    prev.classList.remove('disabled')
+                    next.classList.remove('disabled')
+                }
+        }
+        else {
+            books.innerHTML = `<h1 class="no-results">No Results Found</h1>`
+        }
+
+    })
+}
+
+
+
+
+
+function showBooks(data) {
+
+    main.innerHTML = "";
+    data.forEach(books => {
+        const {volumeInfo,imageLinks} = books;
+        const booksel = document.createElement('div');
+        booksel.classList.add('books');
+         booksel.innerHTML = 
+        
+        `
+        <div class="left-card"><img src=${volumeInfo.imageLinks.thumbnail ? volumeInfo.imageLinks.thumbnail : "assets/img/cover.jpg"}></div>
+        <div class="right-card">${volumeInfo.title.slice(0,30)} <br/><h5>Rating: ${volumeInfo.averageRating ? volumeInfo.averageRating+"/5" : 'Not Rated'}</h5>  <a href ='card.html?id=${books.id}' dataset-id=${books.id} class="btn">View More</a>  </div>
+         
+       
+      `
+
+        main.appendChild(booksel);
+    });
+
+
+}
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+  
+  
+    console.log(searchTerm)
+    if (searchTerm) {
+        currentPage=1;
+         current.innerText = 1;
+        startIndex=0;
+        getFiction(searchURL + searchTerm  + '&key=AIzaSyABCMotJsaZVpPDKaB03v72YRoWAkmiuvo&searchIndex=0');
+    }
+})
+
+
+next.addEventListener('click', () => {
+    if(startIndex < totalPages){
+  startIndex = startIndex + maxResults;
+  currentPage +=1;
+   current.innerText = currentPage;
+}
+pageCall(startIndex);
+
+
+})
+
+prev.addEventListener('click', () => {
+    if (startIndex >= 0 && currentPage>1) {
+         startIndex = startIndex - maxResults;
+         currentPage -=1;
+          current.innerText = currentPage;
+    }
+    pageCall(startIndex);
+
+
+})
+// pageCall();
+function pageCall(page) {
+    let urlSplit = lastUrl.split('?');
+    let queryParams = urlSplit[1].split('&');
+    // console.log(queryParams)
+     let key = queryParams[queryParams.length - 1].split('=');
+    // console.log(key)
+    if (key[0] != 'startIndex') {
+        let url = lastUrl + '&startIndex=' + page;
+         getFiction(url);
+    }
+    else {
+        key[1] = page.toString();
+        console.log(key[1])
+        let a = key.join('=');
+        console.log(a)
+        queryParams[queryParams.length - 1] = a;
+        let b = queryParams.join('&');
+        let url = urlSplit[0] + '?' + b;
+        console.log(url)
+         getFiction(url);
+    }
+}
+
+
+var textWrapper = document.querySelector('.ml6 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
